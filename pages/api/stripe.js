199ace3174535eb30bc0e52de8ad6db617c57199
Ratau45 <71636622+Ratau45@users.noreply.github.com>
2022-08-05 13:33:21 +0200

    changed success

--- pages/api/stripe.js
+++ pages/api/stripe.js
@@ -33,7 +33,7 @@ export default async function handler(req, res) {
           quantity: item.quantity
         }
         }),
-          success_url: `${req.headers.origin}/success`,
+          success_url: `${req.headers.origin}/Success`,
           cancel_url: `${req.headers.origin}/canceled`,
       }
     // Create Checkout Sessions from body params.
--- pages/success.js
+++ pages/success.js
@@ -4,7 +4,7 @@ import {BsBagCheckFill} from 'react-icons/bs';
 import { useStateContext } from '../context/StateContext';
 import { runFireworks } from './components/Lib/utils';
 
-const success = () => {
+const Success = () => {
  const {setCartItems, settotalPrice, settotalQuantities} = useStateContext();
 
   useEffect(() => {
@@ -40,4 +40,4 @@ const success = () => {
   )
 }
 
-export default success
+export default Success
